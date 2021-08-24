ba5dd7d44ed74e8d4447ef1ddf36610b3f95b36c
Praveen <bhutanipraveen74@gmail.com>
2021-08-24 16:41:05 +0530

    signup added

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,8 +1,105 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { firebaseDb, firebaseStorage } from '../config/firebase';
+import { AuthContext } from '../context/AuthProvider';
 
 
-const SignUp = () => {
-    return ( <div>HHH</div> );
+const SignUp = (props) => {
+
+    
+        const [username, setusername] = useState("");
+        const [email, setemail] = useState("");
+        const [password, setpasswrd] = useState("");
+        const [profileImage,setProfileImage]=useState(null);
+        const [message,setmessage]=useState("");
+        const{signUp} =useContext(AuthContext);
+
+      const  handleFileSubmit=(e)=>{
+          let fileObject=e.target.files[0];
+          setProfileImage(fileObject);
+
+        }
+       const handleSignUp= async()=>{
+       
+        try{
+            let response=await signUp(email,password);
+            let uid=response.user.uid;
+
+           const uploadPhotoObject= firebaseStorage.ref(`/profilePhotos/${uid}/image.jpg`).put(profileImage);
+
+           uploadPhotoObject.on('state_changed',fun1,fun2,fun3);
+
+           function fun1(snapshot){
+
+                let progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                console.log(progress);
+
+           }
+           function fun2(error){
+               console.log(error);
+        }
+       async function fun3(){
+               
+            let photoUrl= await uploadPhotoObject.snapshot.ref.getDownloadURL();
+            firebaseDb.collection("users").doc(uid).set({
+                email:email,
+                userId:uid,
+                username:username,
+                photoUrl:photoUrl
+            })
+            props.history.push("/");
+        }
+        }
+        catch(err){
+            console.log(err);
+            setmessage(err.message);
+        }
+        }
+
+    return (
+
+        <>
+            <h1>Login Page</h1>
+            <div>
+            <div>
+                    Username
+                    <input
+                        value={username}
+                        onChange={(e) => setusername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    Email
+                    <input
+                        value={email}
+                        onChange={(e) => setemail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    Password
+                    <input
+                        value={password}
+                        onChange={(e) => setpasswrd(e.target.value)}
+
+                    />
+                </div>
+                <div>
+                    Profile-Photo
+                    <input
+                       type="file"
+                       accept="image/*"
+                       onChange={(e)=>{handleFileSubmit(e);
+                    }}
+
+                    />
+                </div>
+            </div>
+             <button onClick={handleSignUp}>SignUp</button>
+                <h2 style={{color:"red"}}>{message}</h2> 
+        </>
+
+
+
+    );
 }
- 
+
 export default SignUp;
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,17 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCWGMieR9ciIs6MJG_-z5sgtuwQBSKk1pM",
-    authDomain: "login-page-52229.firebaseapp.com",
-    projectId: "login-page-52229",
-    storageBucket: "login-page-52229.appspot.com",
-    messagingSenderId: "999131672644",
-    appId: "1:999131672644:web:cb060327e0110bc0cc8c11"
+    apiKey: "AIzaSyBULm8WLaVosGvPekucr5RHnbWfsus3Slo",
+    authDomain: "reels-794a4.firebaseapp.com",
+    projectId: "reels-794a4",
+    storageBucket: "reels-794a4.appspot.com",
+    messagingSenderId: "80706255488",
+    appId: "1:80706255488:web:ac07f8f354d9d912ed8465"
   };
 
  
 
   let firebaseApp = firebase.initializeApp(firebaseConfig);
   export let firebaseAuth = firebaseApp.auth();
+  export let firebaseStorage=firebaseApp.storage();
+  export let firebaseDb=firebaseApp.firestore();
