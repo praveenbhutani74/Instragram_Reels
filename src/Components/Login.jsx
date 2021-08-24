fb5e71a76dafe38607cd0c1e875bd2f577cf60b7
Praveen <bhutanipraveen74@gmail.com>
2021-08-24 13:25:24 +0530

    Login Logout done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthProvider';
 
 
-const Login = () => {
-    return ( <h1>Hello</h1> );
+const Login = (props) => {
+    const [email, setemail] = useState("");
+    const [password, setpasswrd] = useState("");
+    let { login } = useContext(AuthContext);
+    const[message,setmessage]=useState("");
+
+    const handleLogin= async(e)=>{
+        try{
+        await login(email,password);
+        props.history.push("/");
+        }
+        catch(err){
+            setmessage(err.message);
+            setemail("");
+            setpasswrd("");
+
+        }
+    }
+
+    return (
+        <>
+            <h1>Login Page</h1>
+
+            <div>
+                <div>
+
+                    Email
+                    <input
+                        value={email}
+                        onChange={(e) => setemail(e.target.value)}
+
+                    />
+                </div>
+                <div>
+
+                    Password
+                    <input
+                        value={password}
+                        onChange={(e) => setpasswrd(e.target.value)}
+
+                    />
+                </div>
+            </div>
+            <button onClick={handleLogin}>Login</button>
+            <h2 style={{color:"red"}}>{message}</h2>
+        </>
+
+    );
 }
- 
+
 export default Login;
